e2d1ebeba01c5dc235a613f258b4a34cc3003497
Stanislav Golovin <golovin.ds93@gmail.com>
2023-01-10 22:41:16 +0300

    solved

--- task_1.py
+++ task_1.py
@@ -1,5 +1,3 @@
-import sys
-
 
 class FlatIterator:
     def __init__(self, list_of_list):
@@ -7,17 +5,17 @@ class FlatIterator:
 
     def __iter__(self):
         self.main_list_cursor = 0
-        self.nested_list_cursor = -1
+        self.nested_list_cursor = 0
         return self
 
     def __next__(self):
-        self.nested_list_cursor += 1  # [1][2]
-        if self.nested_list_cursor is IndexError:
+        self.nested_list_cursor += 1  # [1][0]
+        if self.nested_list_cursor > len(self.main_list[self.main_list_cursor]):
             self.main_list_cursor += 1
-            self.nested_list_cursor = 0
-        if self.main_list_cursor is IndexError:
+            self.nested_list_cursor = 1
+        if self.main_list_cursor == len(self.main_list):
             raise StopIteration
-        item = self.main_list[self.main_list_cursor][self.nested_list_cursor]
+        item = self.main_list[self.main_list_cursor][self.nested_list_cursor - 1]
         return item
 
 
@@ -28,10 +26,6 @@ def test_1():
         [1, 2, None]
     ]
 
-    for item in FlatIterator(list_of_lists_1):
-        print(item)
-
-    sys.exit()
     for flat_iterator_item, check_item in zip(
             FlatIterator(list_of_lists_1),
             ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
